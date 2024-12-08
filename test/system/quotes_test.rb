db91b30f8a1f8949c27ac819852960f655aa55cc
nobu09 <11857481+nobu09@users.noreply.github.com>
2024-12-09 06:51:37 +0900

    add system spec

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -6,4 +6,15 @@ class QuotesTest < ApplicationSystemTestCase
   #
   #   assert_selector "h1", text: "Quotes"
   # end
+  visit quotes_path
+  assert_selector "h1", text: "Quotes"
+
+  click_on "New quote"
+  assert_selector "h1", text: "New quote"
+
+  fill_in "Name", with: "Capybara Quote"
+  click_on "Create quote"
+
+  assert_selector "h1", text: "Quotes"
+  assert_text "Capybara quote"
 end
