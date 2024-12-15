fef9dd0a852675e5047bf21bfbf216d17772bd3a
nobu09 <11857481+nobu09@users.noreply.github.com>
2024-12-16 08:41:28 +0900

    headless_chromeについて注意書きのコメント

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,6 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  # headless_chrome 指定だとテスト時にChromeが開かない
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
 end
