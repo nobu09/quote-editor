d874bd97db83ec3be38543ccca7e6f98c767476f
nobu09 <11857481+nobu09@users.noreply.github.com>
2025-01-08 08:03:17 +0900

    before_action sleep 3を取り除いた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
-  # Add this line to see the progress bar long enough
-  # and remove it when it has the expected styles
-  before_action -> { sleep 3 }
 end
